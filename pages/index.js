c5fcd0a049cf2f696152a6abc9080db297ec5a38
Kabil-Boutou <kabil.boutou@gmail.com>
2020-05-03 15:26:55 +0000

    chore: nextJs boilerplate ready to use!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
-const Home = () => <div></div>
+const Home = () => (
+    <div>
+        <h1>
+            Next Boilerplate Welcome Page{' '}
+            <span role="img" aria-label="img">
+                🎉
+            </span>
+        </h1>
+    </div>
+)
 
 export default Home
